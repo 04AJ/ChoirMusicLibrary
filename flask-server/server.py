d404635bab6eed42ab0f757cfd4dbde4c674c8aa
Alan Johnson <alanjcd2004@gmail.com>
2022-09-11 16:38:11 -0500

    connected to flask server

---
+++ flask-server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Intro Songs API Route
+
+
+@app.route("/intro")
+def intro():
+    return {"introSongs": ["Shine in My Heart", "Come to the Altar", "Every Good Gift"]}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
