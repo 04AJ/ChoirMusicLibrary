b04c2ed99410f1afab0bfb94bb9049f2d429322f
Alan Johnson <alanjcd2004@gmail.com>
2023-03-18 01:20:15 -0500

    using axios to send requests  - needs fixing

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,31 +1,47 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
 function Intro() {
     const [data, setData] = useState([{}]);
 
+    let list = ["/song/1",
+        "/song/2",
+        "/song/3",
+        "/song/4",
+    ];
+
     useEffect(() => {
-        fetch("/song/1").then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data)
-                console.log(data)
-            }
-        )
+
+        const requests = list.map((endpoint) => axios.get(endpoint));
+        axios.all(requests).then((responses) => {
+            responses.forEach((res) => {
+                setData(res.data);
+            })
+        }).catch((error) => {
+            console.error(error);
+        });
     }, [])
+
     return (
         <div>
             <h1>Introduction Songs</h1>
 
-            {(typeof data.introSongs === 'undefined') ? (
+            {(typeof data === 'undefined') ? (
                 <p>Loading...</p>
             ) : (
-                data.introSongs.map((intro, i) => (
-                    <p key={i}>{intro}</p>
-                ))
+
+                <ul>
+                    <li key={data.id}>{data.title} {data.composer} {data.key}</li>
+                </ul>
+
+
+
+
             )}
         </div>
     );
 }
+
 
 export default Intro;
